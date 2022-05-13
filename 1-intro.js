59e208245fd37da54d32c21ca5170ee612da04f4
Duck1s <joeri.breedveld123@gmail.com>
2022-05-13 10:21:32 +0200

    initial commit

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+const amount = 9;
+
+if (amount < 10) {
+	console.log("This is a small number.");
+} else {
+	console.log("This is a large number");
+}
+
+console.log(`Hello, this is my first Node app.`);
