658467060114eb6c17e5e114ba71006d7c634f6b
Duck1s <joeri.breedveld123@gmail.com>
2022-06-01 22:31:12 +0200

    reset 04-store-api and start working on app.js

--- node-express-course/04-store-api/starter/app.js
+++ node-express-course/04-store-api/starter/app.js
@@ -1,41 +1,42 @@
-require('dotenv').config();
-require('express-async-errors');
+require('dotenv').config()
 
-const express = require('express');
-const app = express();
+// asnyc errors
 
-const connectDB = require('./db/connect');
+const express = require('express')
+const app = express()
 
-const productsRouter = require('./routes/products');
+const connectDB = require('./db/connect')
 
-const notFoundMiddleware = require('./middleware/not-found');
-const errorMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 // middleware
-app.use(express.json());
+
+app.use(express.json())
 
 // routes
-app.get('/', (req, res) => {
-	res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
-});
 
-app.use('/api/v1/products', productsRouter);
+app.get('/', (req, res) => {
+	res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
+})
 
 // products route
 
-app.use(notFoundMiddleware);
-app.use(errorMiddleware);
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 const start = async () => {
 	try {
 		// connectDB
-		await connectDB(process.env.MONGO_URI);
-		app.listen(port, console.log(`Server is listening port ${port}...`));
-	} catch (error) {
-		console.log(error);
-	}
-};
 
-start();
+		connectDB(process.env.MONGO_URI)
+
+		app.listen(port, console.log(`Server is listening port ${port}...`))
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+start()
