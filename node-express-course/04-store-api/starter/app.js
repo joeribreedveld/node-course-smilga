3326235b2eff73b778f2ac89e2e428ca78d40c99
Duck1s <joeri.breedveld123@gmail.com>
2022-06-01 23:27:17 +0200

    add: routes models controllers for store api

--- node-express-course/04-store-api/starter/app.js
+++ node-express-course/04-store-api/starter/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+require('express-async-errors')
 
 // asnyc errors
 
@@ -6,6 +7,7 @@ const express = require('express')
 const app = express()
 
 const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
 
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
@@ -19,6 +21,8 @@ app.use(express.json())
 app.get('/', (req, res) => {
 	res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
 })
+
+app.use('/api/v1/products', productsRouter)
 
 // products route
 
