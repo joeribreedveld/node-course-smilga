828792cd64c55004f1a3f4976379b0e1cfc4b009
Duck1s <joeri.breedveld123@gmail.com>
2022-06-04 18:05:49 +0200

    add: company and name search options in product controller

--- node-express-course/04-store-api/starter/controllers/products.js
+++ node-express-course/04-store-api/starter/controllers/products.js
@@ -1,20 +1,30 @@
 const { query } = require('express')
 const Product = require('../models/product')
+const { search } = require('../routes/products')
 
 const getAllProductsStatic = async (req, res) => {
+	const search = 'a'
 	const products = await Product.find({
-		featured: true,
+		name: { $regex: search, $options: 'i' },
 	})
 	res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-	const { featured } = req.query
+	const { featured, company, name } = req.query
 
 	const queryObject = {}
 
 	if (featured) {
 		queryObject.featured = featured === 'true' ? true : false
+	}
+
+	if (company) {
+		queryObject.company = company
+	}
+
+	if (name) {
+		queryObject.name = { $regex: name, $options: 'i' }
 	}
 
 	console.log(queryObject)
